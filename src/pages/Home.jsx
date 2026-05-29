0f0951d0a4309ecef7ad1e0effa4c071f76b4b9a
mtbtv <mtbtv.official.2@gmail.com>
2026-05-30 05:15:12 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -144,7 +144,7 @@ export default function Home() {
       <div className="space-y-12">
         {sections.map((section, rowIndex) => (
           <div
-            clasName="select-none"
+            className="select-none"
             key={section.title}
             ref={(el) =>
               (rowRefs.current[rowIndex] = el)
