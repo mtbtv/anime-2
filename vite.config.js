b270109f24260f3130db1e52d4f9100522cddf5f
mtbtv <mtbtv.official.2@gmail.com>
2026-05-30 12:35:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true // Makes it easy to test on your local network via TV browser
+  }
 });
